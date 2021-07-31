61a359e8fc933b831ff36504d89db5cd4dd6ab38
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-31 16:58:26 +0300

    Added encryption to main App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,8 +11,20 @@ public class App {
         System.out.println("Enter Encryption Key:");
         Integer key = Integer.parseInt(newConsole.readLine());
 
-//        Checks for number digits
         Encrypt newEncryption = new Encrypt(text, key);
+
+//        Checks for number digits in text to be encrypted
+        if(!newEncryption.isString()){
+            System.out.println("Invalid Entry");
+        }
+
+//        checks to see if key is in range of 1 to 25
+        if (!newEncryption.isInRange()){
+            System.out.println("Key Range is between 1 and 25");
+        }
+//        Prints Encrypted text
+        String encryptedText = newEncryption.forward();
+        System.out.println(String.format("CypherText: %s", encryptedText));
 
     }
 }
