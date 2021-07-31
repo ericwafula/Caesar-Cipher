34df516ece952ac89339da1eb9bc267ba212aa48
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-31 14:21:21 +0300

    Added multiple uppercase character decryption logic

--- src/test/java/Decrypt.java
+++ src/test/java/Decrypt.java
@@ -10,8 +10,16 @@ public class Decrypt {
         char[] newCharText = mText.toCharArray();
 
         for (int i = 0; i < newCharText.length; i++){
-            int castText = ((int) newCharText[i] - 97 - this.mKey) % 26 + 97;
-            newCharText[i] = (char)castText;
+            //        For uppercase letters
+            if (Character.isUpperCase(newCharText[i])){
+                int castText = ((int) newCharText[i] - 65 - this.mKey) % 26 + 65;
+                newCharText[i] = (char)castText;
+            }
+            //        For lowercase letters
+            else if (Character.isLowerCase(newCharText[i])){
+                int castText = ((int) newCharText[i] - 97 - this.mKey) % 26 + 97;
+                newCharText[i] = (char)castText;
+            }
         }
 
         final String newS = String.valueOf(newCharText);
